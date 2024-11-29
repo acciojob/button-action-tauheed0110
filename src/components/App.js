acdd5e79a5fc35400be0162b622f6ba2b7f33dc2
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-29 17:55:48 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+  const[flag, setFlag] = useState(true)
+  const handleClick = ()=>{
+    setFlag(!flag)
+  }
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p className={flag ? "hide" : "show"} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      <button id="click" onClick={handleClick}>Click</button>
     </div>
   );
 }
